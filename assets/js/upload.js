ae8f8ce960b2a9558de2cdf889758c4cb1089f9f
Wilfred Denton <dentonw3@gmail.com>
2016-06-03 00:55:31 -0400

    about to switch away from promises to get web worker to do work

--- assets/js/upload.js
+++ assets/js/upload.js
@@ -20,9 +20,16 @@
     }.bind(this));
     return docFrag;
   }
-  var token = window.localStorage.getItem('arkivi-jwt');
-  var previews = document.getElementById('previews');
-  if (token === null) {
+  var state = {
+    token: window.localStorage.getItem('arkivi-jwt')
+  }
+  var refs = {
+    previews: document.getElementById('previews'),
+    dropzone: document.getElementById('dropzone'),
+    icon: document.getElementById('dropzone-icon'),
+    placeholder: document.getElementById('placeholder')
+  }
+  if (state.token === null) {
     window.location.href = '/login';
   }
   var previewTemplate = function (src, name, ext, size, width, height) {
@@ -39,7 +46,7 @@
         ['div', { className: 'preview-img', style: {
           backgroundImage: 'url(' + src + ')'
         }}, ''],
-        ['div', { className: 'preview-progress-bar'}, [
+        ['div', { className: 'preview-progress-bar' }, [
           ['div', { className: 'preview-progress-bar-fluid' }, '']
         ]],
         ['div', { className: 'preview-description row' }, [
@@ -62,7 +69,7 @@
     formData.append('file', file);
     var xhr =  new XMLHttpRequest();
     xhr.open('POST', '/upload-image');
-    xhr.setRequestHeader('Authorization', 'Bearer ' + token);
+    xhr.setRequestHeader('Authorization', 'Bearer ' + state.token);
     xhr.onload = function () {
       progress = 100;
       console.log(progress);
@@ -75,6 +82,8 @@
     }
     xhr.send(formData);
   }
+  var imageWorker = new Worker('/static/js/image-processor.js');
+  
   function resizeBase64Img(base64, ele) {
     var canvas = document.createElement("canvas");
     var width = ele.width;
@@ -92,8 +101,18 @@
     context.drawImage(ele, 0, 0);
     return canvas.toDataURL();
   }
+  var readFile = function (file) {
+    
+    imageWorker.postMessage({type: 'readfile', file: file});
+  }
   var readFilePromise = function (file) {
     return new RSVP.Promise(function (resolve, reject) {
+      // imageWorker.onmessage = function (e) {
+      //   if (e.data.type === 'readfile') {
+      //     resolve(e.data.src);
+      //   }
+      // }
+      // imageWorker.postMessage({type: 'readfile', file: file});
       var reader = new FileReader();
       reader.onload = function (e) {
         resolve(e.target.result);
@@ -115,8 +134,14 @@
         reject('there was an error loading an image');
       }
       img.src = src;
-    })
+    });
   }
+  // var compressImg = function (src) 
+  // imageWorker.addEventListener('message', function (e) {
+  //   if (e.data.type === 'readfile') {
+  //     (e.data.src);
+  //   }
+  // }
   var processFilePromise = function (file) {
     return new RSVP.Promise(function (resolve, reject) {
       var src = '';
@@ -130,60 +155,79 @@
         src = data.src;
         img = data.img;
         var ele = renderTemplate(previewTemplate(src, name, ext, size, img.width, img.height));
-        previews.appendChild(ele);
+        refs.previews.appendChild(ele);
         resolve();
       }).catch(function (err) {
         reject(err);
       });
     })
   }
-  var dropzone = document.getElementById('dropzone');
-  var icon = document.getElementById('dropzone-icon');
-  var previews = document.getElementById('previews');
+ 
+  var busy = false
   var processFiles = function (files) {
+    var numFiles = files.length;
+    var filesCount = 0;
+    var start = new Date();
+    busy = true;
     var formData = new FormData();
     var promises = [];
-    icon.classList.remove('glyphicon-record');
-    icon.classList.add('glyphicon-refresh');
-    icon.classList.add('glyphicon-spin');
+    refs.icon.classList.remove('glyphicon-record');
+    refs.icon.classList.add('glyphicon-refresh');
+    refs.icon.classList.add('glyphicon-spin');
+    refs.placeholder.firstChild.innerHTML = 'processing images...'
+    // Array.prototype.forEach.call(files, function (file) {
+    //   processFile(file);
+    // });
+    // refs.placeholder.addEventListener('fileprocessed', function (e) {
+    // });
     Array.prototype.forEach.call(files, function (file) {
       promises.push(processFilePromise(file));
     });
     RSVP.all(promises).then(function () {
       console.log('finished processing');
-      icon.classList.remove('glyphicon-spin');
-      icon.classList.remove('glyphicon-refresh');
-      icon.classList.add('glyphicon-record');
-      previews.classList.add('show');
+      refs.icon.classList.remove('glyphicon-spin');
+      refs.icon.classList.remove('glyphicon-refresh');
+      refs.icon.classList.add('glyphicon-record');
+      refs.previews.classList.add('show');
+      refs.placeholder.classList.add('slide-away');
+      busy = false;
+      var end = new Date();
+      console.log((end - start) / 1000);
     }).catch(function (err) {
       console.log(err);
+      refs.placeholder.firstChild.innerHTML = 'Drop images above to upload.';
+      busy = false;
     });
   }
-  dropzone.addEventListener('dragover', function (e) {
+  refs.dropzone.addEventListener('dragover', function (e) {
     e.preventDefault();
-    if (!icon.classList.contains('dragover'))
-      icon.classList.add('dragover');
+    if (!refs.icon.classList.contains('dragover'))
+      refs.icon.classList.add('dragover');
     return false;
   });
-  dropzone.addEventListener('dragleave', function (e) {
+  refs.dropzone.addEventListener('dragleave', function (e) {
     e.preventDefault();
-    if (icon.classList.contains('dragover'))
-      icon.classList.remove('dragover');
+    if (refs.icon.classList.contains('dragover'))
+      refs.icon.classList.remove('dragover');
     return false;
   });
-  dropzone.addEventListener('drop', function (e) {
+  refs.dropzone.addEventListener('drop', function (e) {
     e.preventDefault();
-    if (icon.classList.contains('dragover'))
-      icon.classList.remove('dragover');
-    dropzone.classList.add('dropped');
-    processFiles(e.dataTransfer.files);
+    if (refs.icon.classList.contains('dragover'))
+      refs.icon.classList.remove('dragover');
+    refs.dropzone.classList.add('dropped');
+    if (!busy)
+      processFiles(e.dataTransfer.files);
   });
   var filePicker = document.getElementById('dropzone-input');
+  filePicker.addEventListener('click', function (e) {
+    if (busy) e.preventDefault();
+  });
   filePicker.addEventListener('change', function (e) {
-    processFiles(this.files);
+    if (!busy) processFiles(this.files);
   });
   // websocket
-  conn = new WebSocket('ws://' + window.location.host + '/ws?token=' + token);
+  conn = new WebSocket('ws://' + window.location.host + '/ws?token=' + state.token);
   conn.onclose = function(e) {
     console.log('websocket connection closed');
   }
